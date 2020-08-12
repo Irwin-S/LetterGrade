c24f560ba2aac75c9ed8374fc9bc5af86dbf98f0
James Grieve <jamesonrgrieve@gmail.com>
2020-08-12 09:48:49 -0600

    Challenge point - support continued inputs until q is entered;

--- Program.cs
+++ Program.cs
@@ -16,63 +16,73 @@ namespace LetterGrade
             */
 
             double userGrade;
-            string letterGrade;
+            string userInput, letterGrade;
 
             Console.WriteLine("This application will convert a numeric grade to a letter grade.");
 
             Console.Write("Please enter your grade:");
-            userGrade = double.Parse(Console.ReadLine());
+            userInput = Console.ReadLine();
+            while (userInput.ToLower() != "q")
+            {
+                userGrade = double.Parse(userInput);
 
-            if (userGrade >= 97 && userGrade <= 100)
-            {
-                letterGrade = "A+";
-            }
-            else if (userGrade >= 90 && userGrade <= 96)
-            {
-                letterGrade = "A";
-            }
-            else if (userGrade >= 85 && userGrade <= 89)
-            {
-                letterGrade = "A-";
-            }
-            else if (userGrade >= 80 && userGrade <= 84)
-            {
-                letterGrade = "B+";
-            }
-            else if (userGrade >= 75 && userGrade <= 79)
-            {
-                letterGrade = "B";
-            }
-            else if (userGrade >= 70 && userGrade <= 74)
-            {
-                letterGrade = "B-";
-            }
-            else if (userGrade >= 65 && userGrade <= 69)
-            {
-                letterGrade = "C+";
-            }
-            else if (userGrade >= 60 && userGrade <= 64)
-            {
-                letterGrade = "C";
-            }
-            else if (userGrade >= 55 && userGrade <= 59)
-            {
-                letterGrade = "C-";
-            }
-            else if (userGrade >= 50 && userGrade <= 54)
-            {
-                letterGrade = "D";
-            }
-            else if (userGrade >= 0 && userGrade <= 49)
-            {
-                letterGrade = "F";
-            }
-            else
-            {
-                letterGrade = "Invalid";
-            }
+                if (userGrade >= 97 && userGrade <= 100)
+                {
+                    letterGrade = "A+";
+                }
+                else if (userGrade >= 90 && userGrade <= 96)
+                {
+                    letterGrade = "A";
+                }
+                else if (userGrade >= 85 && userGrade <= 89)
+                {
+                    letterGrade = "A-";
+                }
+                else if (userGrade >= 80 && userGrade <= 84)
+                {
+                    letterGrade = "B+";
+                }
+                else if (userGrade >= 75 && userGrade <= 79)
+                {
+                    letterGrade = "B";
+                }
+                else if (userGrade >= 70 && userGrade <= 74)
+                {
+                    letterGrade = "B-";
+                }
+                else if (userGrade >= 65 && userGrade <= 69)
+                {
+                    letterGrade = "C+";
+                }
+                else if (userGrade >= 60 && userGrade <= 64)
+                {
+                    letterGrade = "C";
+                }
+                else if (userGrade >= 55 && userGrade <= 59)
+                {
+                    letterGrade = "C-";
+                }
+                else if (userGrade >= 50 && userGrade <= 54)
+                {
+                    letterGrade = "D";
+                }
+                else if (userGrade >= 0 && userGrade <= 49)
+                {
+                    letterGrade = "F";
+                }
+                else
+                {
+                    letterGrade = "Invalid";
+                }
 
-            Console.WriteLine($"Your grade is {letterGrade}, based on your score of {userGrade}.");
+                Console.WriteLine($"Your grade is {letterGrade}, based on your score of {userGrade}.");
+            
+                Console.Write("Please enter your grade:");
+                userInput = Console.ReadLine();
+            }
+            
+
+            
         }
     }
 }
