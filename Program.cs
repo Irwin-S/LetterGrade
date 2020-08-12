7cceb512b38382c941842979f7b62b3baefbf8d1
James Grieve <jamesonrgrieve@gmail.com>
2020-08-12 09:26:36 -0600

    Initial commit; Variables declared and input parsed;

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LetterGrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userGrade;
+            string letterGrade;
+
+            Console.Write("Please enter your grade:");
+            userGrade = int.Parse(Console.ReadLine());
+
+            
+
+        }
+    }
+}
